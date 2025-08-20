404a86bcff6b9815b405095539f6b0d361acb9af
Bruno Henrique Agria Leite <brunohenrique.9964@gmail.com>
2025-08-19 21:50:18 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -1,10 +1,8 @@
 #Exercicio 1
 #Sistema de estoque com apenas 1 produto 
 #1. Opção de cadastro do nome do produto. 
-#2. Opção de retirar produto do estoque (precisa ver se tem o 
-produto) 
-#3. Opção de adicionar produto no estoque (precisa adicionar 
-um numero maior que 0) 
+#2. Opção de retirar produto do estoque (precisa ver se tem o produto) 
+#3. Opção de adicionar produto no estoque (precisa adicionar um numero maior que 0) 
 #4. Opção de ver a quantidade no estoque 
 
 
@@ -66,4 +64,30 @@ while True:
             
     else:
         print("Opção inválida! Tente novamente.")
-            
+
+
+---
+config:
+  theme: redux
+---
+flowchart TD
+    A(["Inicio"]) --> B["menu"]
+    B --> C["Escolha uma opção:"]
+    C --> n1["1 - Cadastrar produtos"] & n3["2 - Retirar do estoque"] & n5["3 - Adicionar ao estoque"] & n7["4 - Ver quantidade em estoque"]
+    n1 --> n2["Digite o nome do produto"]
+    n3 --> n4["Digite a quantidade a retirar"]
+    n5 --> n6["Digite a quantidade a adicionar"]
+    n7 --> n8["Quantidade em estoque"]
+    n4 --> n9["Há quantidade no estoque ?"]
+    n9 --> n10["Sim"] & n11["Não"]
+    n11 --> n12["Quantidade insuficiente no estoque!"]
+    n10 --> n13["Retirado {retirar} unidade(s). Estoque atual:{quantidad}"]
+    n6 --> n14["Digite a quantidade a adicionar"]
+    n14 --> n15["Menor que 0"] & n16["Maior que 0"]
+    n15 --> n17["A quantidade deve ser maior que zero!"]
+    n16 --> n18["Produto {adicionar} unidade(s). Estoque atual: {quantidade}"]
+    n8 --> n19["Produto: {produto} | Quantidade em estoque: {quantidade}"]
+    C@{ shape: diam}
+    n9@{ shape: diam}
+    n13@{ shape: rect}
+    n14@{ shape: diam}
