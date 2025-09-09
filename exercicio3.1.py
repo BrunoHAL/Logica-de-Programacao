cd2d1145d223255cc288911d731cb5dcc51af28e
Bruno Henrique Agria Leite <brunohenrique.9964@gmail.com>
2025-09-09 20:06:10 -0300

    Add files via upload

---
+++ exercicio3.1.py
@@ -0,0 +1,133 @@
+# Criar class publicacao (conteudo, descricao, autor, data)
+# Criar lista_de_publicacao[]
+# Def menu[]
+# Criar outras modalidades
+
+from dataclasses import dataclass
+from datetime import datetime
+
+#criação de classe de publicação
+@dataclass
+class Publicacao:
+    conteudo: str
+    descricao: str
+    autor: str
+    data_hora: datetime
+    curtidas: int = 0
+    
+lista_publicacoes = []
+
+#Aqui estamos criando uma publicação
+def criar_publicacao():
+    print("\n === CRIAR PUBLICACÃO ===")
+    conteudo = input("Digite o conteúdo da publicação: ")
+    descricao = input("Digite a descrição: ")
+    autor = input("Digite o nome do autor: ")
+    data_hora = datetime.now()
+    
+    nova_publicacao = Publicacao(conteudo, descricao, autor, data_hora)
+    lista_publicacoes.append(nova_publicacao)
+    print("Publicação criada com sucesso!")
+
+#Aqui estou criando a opção de curtida na publicação    
+def curtir_publicacao():
+    print("\n=== CURTIR PUBLICAÇÃO ===")
+    if not lista_publicacoes:
+        print("Nenhuma publicação disponível.")
+        return
+
+    visualizar_feed()
+    try:
+        indice = int(input("Digite o número da pulicação para curtir: ")) - 1
+        if 0 <= indice < len(lista_publicacoes):
+            lista_publicacoes[indice].curtidas += 1
+            print("Publicação curtida!")
+        else:
+            print("Publicação não encontrada.")
+    except ValueError:
+        print("Número inválido.")
+ 
+#Aqui estou desenvolvendo opção de visualização       
+def visualizar_feed():
+    print("\n=== FEED ===")
+    if not lista_publicacoes:
+        print("Nenhuma publicação disponível.")
+        return
+    for i, pub in enumerate(lista_publicacoes, 1):
+        print(f"{i}. {pub.autor} - {pub.curtidas} curtidas")
+        print(f"    {pub.conteudo[:50]}...")
+        print(f"    {pub.data_hora.strftime('%d/%m/%Y %H:%M')}")
+        print("-" * 40)
+
+# Aqui estou criando o sistema de visualizar publicação avulso       
+def visualizar_publicacao_individual():
+    print("\n=== VISUALIZAR PUBLICAÇÃO ===")
+    if not lista_publicacoes:
+        print("Nenhuma publicação disponível.")
+        return
+    
+    visualizar_feed()
+    try:
+        indice = int(input("Digite o número da publicação: ")) - 1
+        if 0 <= indice < len(lista_publicacoes):
+            pub = lista_publicacoes[indice]
+            print(f"\nAutor: {pub.autor}")
+            print(f"Data: {pub.data_hora.strftime('%d/%m/%Y %H:%M')}")
+            print(f"Conteúdo: {pub.conteudo}")
+            print(f"Descrição: {pub.descricao}")
+            print(f"Curtidas: {pub.curtidas}")
+        else:
+            print("Publicação não encontrada.")
+    except ValueError:
+        print("Número inválido.")
+
+# Aqui será definido o tipo de visualização por autor    
+def visualizar_publicacoes_por_autor():
+    print("\n=== PUBLICAÇÕES POR AUTOR ===")
+    if not lista_publicacoes:
+        print("Nenhuma publicação disponível")
+        return
+    
+    autor = input("Digite o nome do Autor: ")
+    publicacoes_autor = [pub for pub in lista_publicacoes if pub.autor.lower() == autor.lower]
+    
+    if not publicacoes_autor:
+        print(f"Nenhuma publicação encontrada para {autor}.")
+        return
+    
+    print(f"\nPublicações de {autor}:")
+    for pub in publicacoes_autor:
+        print(f"- {pub.conteudo[:50]}... ({pub.curtidas} curtidas)")
+        print(f" {pub.data_hora.strftime('%d/%m/%Y %H:%M')}")
+        print("-" * 30)
+        
+# Criação do menu para ser mostrado.    
+def menu():
+    while True:
+        print("\n REDE SOCIAL ===")
+        print("1. Criar Publicação")
+        print("2. Curtir Publicação")
+        print("3. Visualizar Feed")
+        print("4. Visualizar Publicação Individual")
+        print("5. Visualizar Publicações por Autor")
+        print("0. Sair")
+        
+        opcao = input("Escolha uma opção: ")
+        
+        if opcao == "1":
+            criar_publicacao()
+        elif opcao == "2":
+            curtir_publicacao()
+        elif opcao == "3":
+            visualizar_feed()
+        elif opcao == "4":
+            visualizar_publicacao_individual()
+        elif opcao == "5":
+            visualizar_publicacoes_por_autor()
+        elif opcao == "0":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida!")
+if __name__ == "__main__": 
+    menu()
